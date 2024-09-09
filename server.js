32663926db0840402c0d1c3ad71a642a1ad72dc0
Ka1que <kaiquegodoys@gmail.com>
2024-09-09 00:45:34 -0300

    Crud Create -Main

---
+++ server.js
@@ -0,0 +1,34 @@
+// import { createServer } from 'node:http'
+
+
+// const server = createServer((req, res) => {
+//     res.write('oi')
+//     res.write('<h1>TESTE</h1>')
+//     return res.end()
+// })
+
+// server.listen(1212)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.post('/home/post', ()=> {
+    return 'Hello'
+})
+
+server.get('/home', ()=> {
+    return 'Hello get'
+})
+
+server.put('/home/put', ()=> {
+    return 'Hello put'
+})
+
+server.delete('/home/del', ()=> {
+    return 'Hello del'
+})
+
+server.listen({
+    port: 1212
+})
